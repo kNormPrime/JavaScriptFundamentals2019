5785754ff2b38f5acf8a5883325adc3f6707bcf6
kNormPrime <knormile@albany.edu>
2019-11-20 15:56:01 -0500

    async-await is working

--- exercises/11-async-await/async-await.js
+++ exercises/11-async-await/async-await.js
@@ -9,3 +9,26 @@
  * This is the API you will be using. The method will be GET.
  * https://ron-swanson-quotes.herokuapp.com/v2/quotes
  */
+
+
+///////////////// BUTTON ACTION w/ async-await //////////////////////////////////////////////
+const submitBUTTON = document.querySelector('[type="button"]') ;
+submitBUTTON.addEventListener("click", async () =>
+{    
+  let wantedQUOTE ;
+
+  try {
+     const result = await axios.get('https://ron-swanson-quotes.herokuapp.com/v2/quotes');
+     wantedQUOTE= (result.data) ;   return submitBUTTON.textContent = wantedQUOTE;
+      }
+
+  catch (err) 
+      { 
+    wantedQUOTE = err; 
+    return submitBUTTON.textContent('An error occured:'+ wantedQUOTE); 
+      } ; 
+   
+} ) ;
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+
